41e8e48649ec5cf1db5785c0628561c63659fb67
sethah <seth.hendrickson16@gmail.com>
2015-01-17 21:11:57 -0800

    more queries built

--- queries.py
+++ queries.py
@@ -2,8 +2,6 @@ import psycopg2
 import psycopg2.extras
 from datetime import datetime, date
 import db_tools
-import matplotlib.pyplot as plt
-import pandas as pd
 
 
 class Query(object):
@@ -35,9 +33,15 @@ class Query(object):
             if commit:
                 conn.commit()
         except:
+            print 'Error executing query'
             conn.rollback()
 
-        self.results = cur.fetchall()
+        # fetch results if there are some
+        try:
+            self.results = cur.fetchall()
+        except psycopg2.ProgrammingError:
+            pass
+
         conn.close()
 
     def show_results(self):
@@ -66,6 +70,19 @@ class Query(object):
 
         self.query = q
 
+    def box_stats(self, the_date):
+        date_string = datetime.strftime(the_date, '%Y-%m-%d')
+        date_seq = datetime.strftime(the_date, '%Y%m%d')
+        start_date = self.get_min_date(the_date)
+        q = """CREATE TABLE box_stats_%s AS
+               (SELECT b.*, g.dt
+               FROM box_stats b
+               JOIN games g
+               ON g.id = b.gameid AND (g.dt BETWEEN '%s' AND '%s'))
+            """ % (date_seq, start_date, date_string)
+
+        self.query = q
+
     def home_win_pct(self):
         q = """SELECT CAST(COUNT(*) AS REAL) / (SELECT COUNT(*) FROM games2)
                FROM games2
@@ -74,56 +91,70 @@ class Query(object):
 
         self.query = q
 
-    def aggregate_box(self, date):
-        date_string = datetime.strftime(date, '%Y-%m-%d')
-        q = """ SELECT
-                    SUM(pts) as pts,
-                    SUM(fgm) as fgm,
-                    SUM(fga) as fga,
-                    SUM(tpm) as tpm,
-                    SUM(tpa) as tpa,
-                    SUM(ftm) as ftm,
-                    SUM(fta) as fta,
-                    SUM(oreb) as oreb,
-                    SUM(dreb) as dreb,
-                    SUM(reb) as reb,
-                    SUM(ast) as stl,
-                    SUM(blk) as blk,
-                    SUM(turnover) as turnover,
-                    SUM(pf) as pf,
-                    COUNT(teamid) ngames,
-                    teamid
-                FROM box_stats
-                GROUP BY teamid
-            """
+    def aggregate_box(self, the_date):
+        date_string = datetime.strftime(the_date, '%Y-%m-%d')
+        date_seq = datetime.strftime(the_date, '%Y%m%d')
+        start_date = self.get_min_date(the_date)
+        q = """ CREATE TABLE agg_box_stats_%s AS
+                SELECT
+                    SUM(b.pts) as pts,
+                    SUM(b.fgm) as fgm,
+                    SUM(b.fga) as fga,
+                    SUM(b.tpm) as tpm,
+                    SUM(b.tpa) as tpa,
+                    SUM(b.ftm) as ftm,
+                    SUM(b.fta) as fta,
+                    SUM(b.oreb) as oreb,
+                    SUM(b.dreb) as dreb,
+                    SUM(b.reb) as reb,
+                    SUM(b.ast) as stl,
+                    SUM(b.blk) as blk,
+                    SUM(b.turnover) as turnover,
+                    SUM(b.pf) as pf,
+                    COUNT(b.teamid) ngames,
+                    b.teamid,
+                FROM box_stats b
+                JOIN games g
+                ON g.id = b.gameid AND (g.dt BETWEEN '%s' AND '%s')
+                GROUP BY b.teamid
+            """ % (date_seq, datetime.strftime(start_date, '%Y-%m-%d'), date_string)
         self.query = q
+
+    def get_min_date(self, dt):
+        if dt.month < 6:
+            year = dt.year - 1
+        else:
+            year = dt.year
+
+        return date(year, 9, 1)
 
     def box_advanced_stats(self, new_table='advanced_stats',
                            stats_table='box_stats'):
-        prefix = """CREATE TABLE '%s' AS""" % new_table
+        prefix = """CREATE TABLE %s AS""" % new_table
 
         opp = """opp AS
                  (SELECT
                     b.gameid,
                     a.teamid,
                     b.teamid as oppid
-                  FROM '%s' a
-                  JOIN '%s' b
+                  FROM %s a
+                  JOIN %s b
                   ON a.gameid = b.gameid AND a.teamid != b.teamid),
-              """ % stats_table
+              """ % (stats_table, stats_table)
         pos = """pos AS
                 (SELECT 
+                    dt,
                     gameid,
                     teamid,
                     fga - oreb + turnover + 0.475*fta AS pos
-                 FROM '%s'),
+                 FROM %s),
               """ % stats_table
         ppp = """ppp AS
                     (SELECT
                         b.gameid,
                         b.pts / p.pos AS ppp,
                         b.teamid
-                     FROM '%s' b
+                     FROM %s b
                      JOIN pos p
                      ON b.gameid = p.gameid AND b.teamid = p.teamid),
               """ % stats_table
@@ -141,26 +172,26 @@ class Query(object):
                         (fgm + 0.5*tpm) / fga AS efg,
                         teamid,
                         gameid
-                     FROM '%s'),
+                     FROM %s),
               """ % stats_table
         ort = """ort AS
                     (SELECT
                         CAST (oreb AS REAL) / (fga - fgm) AS ort,
                         teamid,
                         gameid
-                     FROM '%s'),
+                     FROM %s),
               """ % stats_table
         topp = """topp AS
                     (SELECT
                         b.gameid,
                         b.turnover / p.pos AS topp,
                         b.teamid
-                     FROM '%s' b
+                     FROM %s b
                      JOIN pos p
                      ON b.gameid = p.gameid AND b.teamid = p.teamid)
              """ % stats_table
 
-        suffix = '''SELECT pos.gameid, pos.teamid,
+        suffix = '''SELECT pos.gameid, pos.teamid, pos.dt,
                           (SELECT opp.oppid
                               FROM opp 
                               WHERE (opp.gameid = pos.gameid)
@@ -196,9 +227,16 @@ class Query(object):
 
 def main():
     q = Query()
-    q.aggregate_box(date(2014, 3, 3))
-    q.execute(limit=10)
-    q.show_results()
+    # q.aggregate_box(date(2013, 12, 1))
+    # q.execute(commit=True)
+    # return None
+    the_date = date(2013, 12, 1)
+    date_seq = datetime.strftime(the_date, '%Y%m%d')
+    q.box_advanced_stats('advanced_stats_%s' % date_seq,'box_stats_%s' % date_seq)
+    #q.box_stats(the_date)
+    q.execute(commit=True)
+    print q.query
+    #q.show_results()
 
 
 if __name__ == '__main__':
